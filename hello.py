1a56d75260474bdf75bed17a2c67950eb08f87cb
Aleksei Kosianenko <lehakos@gmail.com>
2017-10-10 18:13:06 +0400

    exercise 2: fix

--- hello.py
+++ hello.py
@@ -5,11 +5,11 @@ def app(evniron, start_response):
   headers = [
     ('Content-Type', 'text/plain')
   ]
-  body = []
+  body = ''
   parsed_query = parse_qsl(evniron['QUERY_STRING'])
   
   for item in parsed_query:
-    body.append('{}={}\n'.format(item[0], item[1]))
+    body += '{}={}\n'.format(item[0], item[1])
 
   start_response(status, headers)
   return body
