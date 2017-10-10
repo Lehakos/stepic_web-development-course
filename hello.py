228ab5d9300de89db9c9037f101a7854ebafb543
Aleksei Kosianenko <lehakos@gmail.com>
2017-10-10 18:43:14 +0400

    exercise 2: fix for test

--- hello.py
+++ hello.py
@@ -1,15 +1,9 @@
-from urllib.parse import parse_qsl
-
 def app(evniron, start_response):
   status = '200 OK'
   headers = [
     ('Content-Type', 'text/plain')
   ]
-  body = ''
-  parsed_query = parse_qsl(evniron['QUERY_STRING'])
-  
-  for item in parsed_query:
-    body += '{}={}\n'.format(item[0], item[1])
+  body = evniron['QUERY_STRING'].replace('&', '\n')
 
   start_response(status, headers)
   return [ bytes(body, 'utf-8') ]
