b1e516e01fd96c1dcabe833b4c84790d9a03f11b
Aleksei Kosianenko <lehakos@gmail.com>
2017-10-12 15:22:58 +0400

    exercise 3: stupid python and ubuntu and all it world!!!!!!! And stepic too

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip3 install virtualenv --user
-virtualenv -p python3 env
+virtualenv -p python3 env --no-site-packages
 source env/bin/activate
 pip install -r requirements.txt
 sudo rm -rf /etc/nginx/sites-enabled/default
